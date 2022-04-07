a29a586dec224edc1fbe8cfe23a6c4a497e9df74
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-07 11:33:27 -0400

    commit tree 91b6b89618d452e341a2132283afcacc9dd6b2e3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record){
+    let win = record.find(element => element.result === 'W')
+    if (win){
+        return win.year
+    }
+    return
+}
